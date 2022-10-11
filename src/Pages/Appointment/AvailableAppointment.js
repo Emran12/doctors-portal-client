f362a82a32efb448cf0766b1e42e44bf21409c19
Emran12 <emran6452@gmail.com>
2022-10-11 11:08:48 +0600

    modal added

--- src/Pages/Appointment/AvailableAppointment.js
+++ src/Pages/Appointment/AvailableAppointment.js
@@ -1,9 +1,12 @@
 import { format } from "date-fns";
 import React from "react";
 import { useEffect, useState } from "react";
+import BookingModal from "./BookingModal";
+import Service from "./Service";
 
 const AvailableAppointment = ({ date }) => {
   const [services, setServices] = useState([]);
+  const [treatment, setTreatment] = useState(null);
   useEffect(() => {
     fetch("services.json")
       .then((res) => res.json())
@@ -14,7 +17,22 @@ const AvailableAppointment = ({ date }) => {
       <h1 className="text-xl text-secondary text-center mt-10">
         Available services on {format(date, "PP")}
       </h1>
-      <div>{services.length}</div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  gap-5">
+        {services.map((service) => (
+          <Service
+            key={service._id}
+            service={service}
+            setTreatment={setTreatment}
+          ></Service>
+        ))}
+      </div>
+      {treatment && (
+        <BookingModal
+          treatment={treatment}
+          date={date}
+          setTreatment={setTreatment}
+        ></BookingModal>
+      )}
     </div>
   );
 };
--- src/Pages/Appointment/Service.js
+++ src/Pages/Appointment/Service.js
@@ -1,6 +1,33 @@
 import React from "react";
-const Service = () => {
-  return <div>service</div>;
+const Service = ({ service, setTreatment }) => {
+  const { name, slots } = service;
+
+  return (
+    <div>
+      <div className="card  bg-base-100 shadow-xl ">
+        <div className="card-body">
+          <h2 className="card-title">{name}</h2>
+          <p>
+            {slots.length < 1 ? (
+              <span className="text-red-500">service not available</span>
+            ) : (
+              <span>{slots[0]}</span>
+            )}
+          </p>
+          <div className="card-actions justify-center">
+            <label
+              for="booking-modal"
+              disabled={slots.length === 0}
+              onClick={() => setTreatment(service)}
+              className="btn btn-secondary text-white uppercase"
+            >
+              Book Appointment
+            </label>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Service;
